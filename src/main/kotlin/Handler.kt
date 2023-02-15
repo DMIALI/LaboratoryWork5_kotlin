de4945059c9ccde6aa0c8d1c56c92d7b166ebee4
Dmitrii <alievd08@mail.ru>
2023-02-16 01:22:34 +0300

    processing supported commands

--- src/main/kotlin/Handler.kt
+++ src/main/kotlin/Handler.kt
@@ -12,7 +12,55 @@ class Handler {
             while(command != "exit"){
                 command = scanner.nextLine()
                 when(command){
-                    "help" -> println("Usfull information")
+                    "help"->{
+
+                    }
+                    "info"->{
+
+                    }
+                    "show"->{
+
+                    }
+                    "add {element}"->{
+
+                    }
+                    "update id {element}"->{
+
+                    }
+                    "remove_by_id id"->{
+
+                    }
+                    "clear"->{
+
+                    }
+                    "save"->{
+
+                    }
+                    "execute_script file_name"->{
+
+                    }
+                    "exit"->{
+                        println("Goodbye :)")
+                    }
+                    "remove_at index"->{
+
+                    }
+                    "shuffle"->{
+
+                    }
+                    "reorder"->{
+
+                    }
+                    "remove_any_by_front_man frontMan"->{
+
+                    }
+                    "count_by_number_of_participants numberOfParticipants"->{
+
+                    }
+                    "filter_by_albums_count albumsCount"->{
+
+                    }
+
                     else -> println("no such command")
                 }
                 //val command = scanner.nextLine()
