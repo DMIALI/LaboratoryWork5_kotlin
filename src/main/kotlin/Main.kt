a4bf9767c4ac46050bb8c2575902c973b5420e84
Vasilii <vasilev_vasily_123@mail.ru>
2023-02-15 14:20:30 +0300

    test commit 2

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,12 +2,6 @@ import java.util.Scanner
 
 fun main(args: Array<String>) {
     println("Hello World!")
-    val scanner = Scanner(System.`in`)
-    var command = ""
-    while (!command.equals("exit")){
-        command = scanner.nextLine()
-        print (command)
-    }
     // Try adding program arguments via Run/Debug configuration.
     // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
     println("Program arguments: ${args.joinToString()}")
