66a4710cd25f5c5972e79cffcb7db7c5c7b36504
Dmitrii <alievd08@mail.ru>
2023-02-16 14:57:20 +0300

    delete comments of old code

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -5,17 +5,4 @@ fun main(args: Array<String>) {
 
     val menu = Handler()
     menu.CommandsMenu()
-    /*val scanner = Scanner(System.`in`)
-    var command = ""
-    while (!command.equals("exit")){
-        command = scanner.nextLine()
-        print (command)
-    }
-
-
-    val a = scanner.nextLine()
-    println(a)*/
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    //println("Program arguments: ${args.joinToString()}")
 }
